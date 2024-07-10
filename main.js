c7a74a53bd7fe53385a544e452f82eb8eaf6d069
Lesya <lesia.str.05@gmail.com>
2024-07-10 18:49:47 +0200

    add js and mobile rules

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let buttonClose = document.getElementById('lol');
 let rules = document.getElementById('rulesDesktop');
 let showRules = document.getElementById('show');
@@ -5,10 +6,6 @@ let showRules = document.getElementById('show');
 showRules.addEventListener("click", function(){
     rules.style.display = 'flex';
 })
-
-buttonClose.onclick = function(){
-    rules.showModal();
-}
 
 buttonClose.addEventListener("click", function() {
     rules.style.display = 'none';
